d220b222316a083c0f9397a21e99d7da40c896ec
Isak Simonsson <isak.simonsson@gmail.com>
2025-02-15 17:46:25 +0100

    Csv handler

--- app/src/main/java/com/example/myfirstapplication/ui/game/CsvHandler.java
+++ app/src/main/java/com/example/myfirstapplication/ui/game/CsvHandler.java
@@ -6,9 +6,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import android.content.Context;
 
 public class CsvHandler {
